20243e92b62ad3af0c9ef831f9aa193b5b2943c8
Eyob <eabebe91@gmail.com>
2025-10-20 11:05:29 +0300

    dashboard skeleton added

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import LoginPage from "./pages/login-page.tsx";
 import NotFound from "./pages/not-found.tsx";
+import DashboardLayout from "./pages/dashboard-layout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -13,7 +14,9 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />} />
         <Route path="auth">
           <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<div>Register Page</div>} />
+        </Route>
+        <Route path="dashboard" element={<DashboardLayout />}>
+          <Route index element={<div>Dashboard Page</div>} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ src/pages/dashboard-layout.tsx
@@ -0,0 +1,54 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { Button } from "@/components/ui/button";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Bell, Search } from "lucide-react";
+import { Outlet } from "react-router";
+
+export default function DashboardLayout() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-14 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-14">
+          <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mx-2 data-[orientation=vertical]:h-4"
+            />
+            <InputGroup className="sm:w-sm md:w-md lg:w-lg w-xl ml-auto flex items-center">
+              <InputGroupInput placeholder="Search..." />
+              <InputGroupAddon>
+                <Search />
+              </InputGroupAddon>
+              <InputGroupAddon align="inline-end">12 results</InputGroupAddon>
+            </InputGroup>
+            <div className="ml-auto flex items-center gap-2">
+              <Button
+                variant="outline"
+                asChild
+                size="icon-lg"
+                className="hidden sm:flex px-2"
+              >
+                <Bell />
+              </Button>
+            </div>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-2">
+          <Outlet />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
